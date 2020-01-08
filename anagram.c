690375454bb462a04db7c38c978703e8688ffdd2
adel-a <adel.ahm@icloud.com>
2020-01-08 22:43:24 +0400

    First day

---
+++ anagram.c
@@ -0,0 +1,35 @@
+#include<stdio.h>
+#include<string.h>
+
+int factorial(int n);
+
+int main(int argc, char *argv[])
+{
+    int sLength=strlen(argv[1]);
+    int iterations;
+    int i;
+    char buff[sLength+1];
+    char buff2[sLength+1];
+    char buff3[sLength+1];
+    char secLast;
+    char first;
+    strcpy(buff,argv[1]);
+    iterations = factorial(sLength);
+    for (i=0;i<iterations;i++)
+    {
+        secLast=buff[sLength-1];
+        first=buff[0];
+        
+    }
+    
+}
+
+int factorial(int n)
+{
+    int result=n;
+    for (int i = n-1;i > 0;i--)
+    {
+        result = result * i;
+    }
+    return result;
+}
